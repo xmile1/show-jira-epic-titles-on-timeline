e6202d28dae70044a0663c5e54195b370e909809
Uyiosa <hiskonxeptz@gmail.com>
2024-01-12 12:37:04 +0100

    refactor: cleanup code

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,22 +1,5 @@
-<script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
-
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
-}
-</script>
-
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <Logo />
-    <div>Popup</div>
-    <SharedSubtitle />
-
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
-    </div>
+    The Title of Jira Epics should be displayed on Timeline bars
   </main>
 </template>
