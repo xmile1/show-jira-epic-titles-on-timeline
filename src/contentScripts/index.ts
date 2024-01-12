6df77cdd977c17151497829f34fdc24488fd138f
Uyiosa <hiskonxeptz@gmail.com>
2024-01-12 13:55:47 +0100

    update icons

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,27 +1,5 @@
-/* eslint-disable no-console */
-import { onMessage } from 'webext-bridge/content-script'
-
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
 (() => {
-  console.info('[vitesse-webext] Hello world from content script')
-
-  // communication example: send previous tab title from background page
-  onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
-  })
-
-  // mount component to context window
-  const container = document.createElement('div')
-  container.id = __NAME__
-  const root = document.createElement('div')
-  const styleEl = document.createElement('link')
-  const shadowDOM = container.attachShadow?.({ mode: __DEV__ ? 'open' : 'closed' }) || container
-  styleEl.setAttribute('rel', 'stylesheet')
-  styleEl.setAttribute('href', browser.runtime.getURL('dist/contentScripts/style.css'))
-  shadowDOM.appendChild(styleEl)
-  shadowDOM.appendChild(root)
-  document.body.appendChild(container)
-
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
       if (mutation.type === 'childList') {
--- src/manifest.ts
+++ src/manifest.ts
@@ -34,14 +34,6 @@ export async function getManifest() {
       48: './assets/icon-512.png',
       128: './assets/icon-512.png',
     },
-    permissions: [
-      'tabs',
-      'storage',
-      'activeTab',
-    ],
-    host_permissions: [
-      '*://*.atlassian.net/*',
-    ],
     content_scripts: [
       {
         matches: [
